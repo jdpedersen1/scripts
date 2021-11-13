bb619b6feb2135d5b8554a47a2cd0b6fe5d4ba8e
Jacob Pedersen <jdpedersen1@yahoo.com>
2021-11-12 22:29:15 -0600

    updates

--- archman.sh
+++ archman.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
-#test script
+
+#must have awman installed also change browser variable to whichever browser you currently use
+
+#variable for web browser
 browser="qutebrowser"
 
+#prompt user for input as to what application they want information about
 echo "what application?"
+
+#takes user input and creates a variable called application to be called later
 read -r application
 
+#run netcat to check for ingernet connection
 nc -z 8.8.8.8 53  >/dev/null 2>&1
 online=$?
+
+#if internet connection found, launches web browser to arch wiki and fills in application variable with user input from above
 if [ $online -eq 0 ]; then
     "$browser" https://wiki.archlinux.org/title/"$application"
+
+#if no internet connection found, launches awman (command line, offline version of arch wiki) and calls application from user input above
 else
     awman "$application"
 fi
