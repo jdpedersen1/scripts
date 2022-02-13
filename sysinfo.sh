d2c796814c8832bc63826ae7be264180d50350e5
terminalforlife <terminalforlife@yahoo.com>
2022-02-13 18:19:09 +0000

    Overhaul (optimize & fix) sysinfo.sh
    The script was tested and working very fast on my (Ubuntu 18.04-based)
    system, but if you run into any issues, please let me know so I can
    hopefully correct them. Be sure to _test_ these changes before
    accepting them.
    
    I believe these changes require that BASH >= 4.0 be available, which
    most have. I'm not yet sure which version of BASH `<(...)` (process
    substitution) was made available, but a little reading online suggests
    4.0.
    
    * Tidied up header to keep within the 80 column standard
    * Made shebang more portable
    * Preferred BASH over things like AWK, cut(1), and wc(1)
    
      This is part of what makes this PR a huge optimization to this
      script. After these changes, I found this script loaded almost
      instantly, whereas before it took a second or two.
    
    * Prefer `type` builtin over old-school `command` builtin
    
      Just more BASH-y like. The use of the `-P` flag may also help it be
      more reliable, but it should work basically the same, TBH.
    
    * Tidied up some formatting
    
      I went a bit overboard, but blame the OCD. Lol There was mainly some
      botched indenting and trailing spaces which drove me nuts. Fixed now.
    
    * Made pidof(8) more portable
    
      The `-q` flag isn't in my pidof(8) version.
    
    * Removed unneeded tr(1) at end of script
    
      This didn't cause any problems for me. I'm guessing you were having
      issues beforehand, because you weren't parsing away the trailing
      newline character from one of the functions. Should be fine, now.
    
    * Sorted out and prepared for use the package update count function
    
      I have a feeling this was originally a work in progress. It should be
      more useful, now. Just shove in the code for each package manager.
      I might add the code for APT systems in a separate PR or commit.
    
    * Removed superfluous and borked arrays before the `for` loops
    * Somewhat optimized and fixed the fetching of shell versions
    
      I had some issues beforehand. It works better now, and is a bit more
      efficient. Parsing the version strings with BASH would be better, but
      I got lazy.
    
    * Tidied up excessively long and obfuscated one-liner at end of script
    
      It's now much easier to visually parse and handle.
    
    * Replaced subshell with command grouping at end of script
    
      More appropriate and more efficient than using a subshell.

--- sysinfo.sh
+++ sysinfo.sh
@@ -1,95 +1,109 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-############################# !!! IMPORTANT NOTE !!! ###################################
-#                                                                                      #
-#  must run script one time from terminal using ./<path/to/script> to generate the     #
-#  sysinfo.txt file for terminal to display, also if you use the list_updates portion  #
-#  of the script, or create any other function that displays any sort of data that     #
-#  changes, you will want to set the script to run as a cronjob to verify the correct  #
-#  and current information is displayed.                                               #
-#                                                                                      #
-########################################################################################
-
+########################## !!! IMPORTANT NOTE !!! #############################
+#                                                                             #
+#  must run script one time from terminal using ./<path/to/script> to         #
+#  generate the sysinfo.txt file for terminal to display, also if you use     #
+#  the list_updates portion of the script, or create any other function that  #
+#  displays any sort of data that changes, you will want to set the script    #
+#  to run as a cronjob to verify the correct and current information is       #
+#  displayed.                                                                 #
+#                                                                             #
+###############################################################################
 
 ### CHECKS WHICH DISTRO YOU ARE RUNNING
 distro() {
-    awk -F '"' '/PRETTY_NAME/ { print $2 }' /etc/os-release
-}
+    file='/etc/os-release'
+    if [[ -f $file && -r $file ]]; then
+        while IFS='=' read key value; do
+            if [[ $key == PRETTY_NAME ]]; then
+                printf '%s\n' "${value//\"/}"
 
-
-### CHECKS FOR INIT SYSTEM
-### ADD YOUR OWN LINE IF YOU USE OTHER INIT SYSTEM
-find_init() { 
-    if pidof -q systemd;
-    then
-        printf "SystemD"
-    elif [ -f "/sbin/openrc" ];
-    then
-        printf "OpenRC"
-    else 
-        cut -d ' ' -f 1 /proc/1/comm
+                break
+            fi
+        done < "$file"
+    else
+        printf '?'
     fi
 }
 
+### CHECKS FOR INIT SYSTEM
+### ADD YOUR OWN LINE IF YOU USE OTHER INIT SYSTEM
+find_init() {
+    if pidof systemd &> /dev/null; then
+        printf 'SystemD'
+    elif [[ -f /sbin/openrc ]]; then
+        printf 'OpenRC'
+    else
+        file='/proc/1/comm'
+        if [[ -r $file ]]; then
+            read data < "$file"
+            printf '%s' "${data%% *}"
+        else
+            printf '?'
+        fi
+    fi
+}
 
 ### COUNTS NUMBER OF INSTALLED PACKAGES FOR APT PACMAN AND XBPS
 ### ADD YOUR OWN LINE IF YOUR MANAGER IS NOT LISTED
 pkg_count() {
-    pkg_managers=("xbps-install
-        apt
-        pacman")
-        for pkg_mgr in ${pkg_managers[@]}; do
-            if command -v $pkg_mgr &> /dev/null; 
-            then
-                case "$pkg_mgr" in
-                    xbps-install)
-                        xbps-query -l | wc -l
-                        ;;
-                    apt)
-                        apt list --installed &> /dev/null | wc -l
-                        ;;
-                    pacman)
-                        pacman -Q | wc -l
-                        ;;
-                esac
-                return
-            fi
-        done
-        printf "Not Found"
-    }
+    for pkg_mgr in xbps-install apt pacman; do
+        type -P "$pkg_mgr" &> /dev/null || continue
 
+        case $pkg_mgr in
+            xbps-install)
+                readarray lines <(xbps-query -l)
+                printf '%d' ${#lines[*]} ;;
+            apt)
+                while read abbrev _; do
+                    [[ $abbrev == ii ]] && (( line_count++ ))
+                done <<< "$(dpkg -l)"
+                printf '%d' $line_count ;;
+            pacman)
+                readarray lines <(pacman -Q)
+                printf '%d' ${#lines[*]} ;;
+        esac
+        return
+    done
+    printf 'Not Found'
+}
 
 ### CHECKS FOR NUMBER OF UPDATES, ONLY SET UP TO RUN ON VOID
 ### EDIT LINES FOR APT OR PACMAN OR ADD YOUR OWN LINES FOR OTHER MANAGERS
 list_updates() {
-   update_count=("xbps-install
-      apt
-      pacman")
-     for updates in ${update_count[@]}; do 
-        if command -v $updates &> /dev/null;
-        then
-           case "$updates" in
-               xbps-install)
-                  xbps-install -nuM | wc -l
-                  ;;
-              apt)
-                 apt list --installed &> /dev/null | wc -l
-                 ;;
-             pacman)
-                 printf "nope"
-                 ;;
-         esac
-         return
-        fi
-    done
-    printf "Not Found"
-}
+    for pkg_mgr in xbps-install apt pacman; do
+        type -P "$pkg_mgr" &> /dev/null || continue
 
+        #TODO: Unfinished. May need to add code for each package manager.
+        case $pkg_mgr in
+            xbps-install)
+                readarray lines <(xbps-install -nuM)
+                printf '%d' ${#lines[*]} ;;
+            apt)
+                printf '?' ;;
+            pacman)
+                printf '?' ;;
+        esac
+        return
+    done
+    printf 'Not Found'
+}
 
 ### CHECKS WHICH VERSION OF ZSH IS RUNNING, CHANGE FOR BASH OR OTHER SHELLS
 shell() {
-    zsh --version | awk '{ print $1, $2 }'
+    if type -P bash &> /dev/null; then
+        bash -c "printf '%s' \"$BASH_VERSION\""
+    elif type -P zsh &> /dev/null; then
+        zsh -c "printf '%s' \"$ZSH_VERSION\""
+    else
+        printf '?'
+    fi
 }
 
-(printf " [ distro - "; distro; printf "] ["; printf "init sys - "; find_init; printf "] ["; printf "shell - "; shell; printf "] ["; printf "pkgs - "; pkg_count; printf "]") | tr '\n' ' ' > .local/share/sysinfo.txt
-exit
+{
+    printf ' [ LSB: %s ] ' "$(distro)"
+    printf '[ Init: %s ] ' "$(find_init)"
+    printf '[ Shell: %s ] ' "$(shell)"
+    printf '[ PKGs: %s ]' "$(pkg_count)"
+} > .local/share/sysinfo.txt
