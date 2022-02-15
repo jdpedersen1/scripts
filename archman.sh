6516e8ae9f54f6548eff7228f286eb375ef10c63
terminalforlife <terminalforlife@yahoo.com>
2022-02-15 00:30:53 +0000

    Tweak syntax & tidy up a bit
    As you can probably tell, I'm bored.
    
    Tweaked the comments, because they were going pretty much to the other
    end of the monitor, which is a bit nuts for other people reading your
    code. :P
    
    Changed use of `netcat` to `ping`, because it's more portable and
    simpler. If there are any issues or you for some reason _don't_ have
    `ping`, you'll of course need to change it back.
    
    Instead of separately using `$?`, you can just directly test for status
    by putting the command into the `if` statement as the condition,
    because remember, that's basically what an `if` statement, `while`
    loop, and `until` loop is doing; they're checking the status of a
    command.

--- archman.sh
+++ archman.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
 
-#must have awman installed also change browser variable to whichever browser you currently use
+# must have awman installed also change browser variable to whichever browser
+# you currently use
 
-#variable for web browser
+# variable for web browser
 browser="qutebrowser"
 
-#prompt user for input as to what application they want information about
+# prompt user for input as to what application they want information about
 echo "what application?"
 
-#takes user input and creates a variable called application to be called later
-read -r application
+# takes user input and creates a variable called application to be called later
+read application
 
-#run netcat to check for ingernet connection
-nc -z 8.8.8.8 53  >/dev/null 2>&1
-online=$?
-
-#if internet connection found, launches web browser to arch wiki and fills in application variable with user input from above
-if [ $online -eq 0 ]; then
+# if internet connection found, launches web browser to arch wiki and fills in
+# application variable with user input from above
+if ping -qc 1 8.8.8.8 &> /dev/null; then
     "$browser" https://wiki.archlinux.org/title/"$application"
 
-#if no internet connection found, launches awman (command line, offline version of arch wiki) and calls application from user input above
+    # if no internet connection found, launches awman (command line, offline
+    # version of arch wiki) and calls application from user input above
 else
     printf "No Connection"
 fi
-
-
