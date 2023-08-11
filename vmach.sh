230e74e02c6672bf0019718f9a301111e423f421
jake <jake@localhost.localdomain>
2023-08-11 16:30:31 -0500

    Update to vmach.sh script, removed option for efi boot to resolve secure boot issue with tianocore. will update again when I can get it to play nice with secure boot

--- vmach.sh
+++ vmach.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
+#
+## Created By: Jake@Linux
+# Created On: Wed 09 Aug 2023 07:22:28 AM CDT
+# # Project: 
 
 list=$(virsh list --all | cut -b 7- | sed 's/........$//' | tail -n +3 | sed 's/ *$//g')
 
@@ -6,14 +10,30 @@ vms=("Add
 $list
 Quit")
 
+    addvm() {
+        read -rep $'What distro?\n: ' distro
+        read -rep $'How many CPUs?\n: ' cpu
+        read -rep $'How much Mem?\n: ' mem
+        read -rep $'Name of iso?\n: ' iso
+        read -rep $'Disk size?\n: ' gb
+        read -rep $'Type of OS?\n: ' os
+
+
+
+
+        read -r -p "Press Enter to continue" </dev/tty
+
+        virt-install --connect qemu:///system --virt-type qemu --name="$distro" --vcpus="$cpu" --memory="$mem" --cdrom="/home/jake/Desktop/iso/$iso" --disk size="$gb" --os-variant="$os"  &
+    }
+
+
 #choice=$(echo -e "${vms[@]}" | dmenu -fn 'Hermit:size=10' -p 'Virt Machines: ' -nb '#222222' -nf '#b8b8b8' -sb '#005f87' -sf '#000000')
-choice="$(fzf --prompt="Choose a VM : " --border=rounded --margin=5% --color=dark --height 100% --reverse --header=" VM Launcher " --info=hidden --header-first  <<<"${vms[@]}")"
-#notify="$(notify-send "v-machine launcher" "launching $choice")"
+choice="$(fzf --prompt="Choose a VM : " --border=rounded --margin=5% --color=dark --height 100% --reverse --header=" VM Launcher " --info=hidden --header-first <<<"${vms[@]}")"
 
 case "$choice" in
     Add)
-            choice=./.local/scripts/addvm.sh
-            devour alacritty -e $choice
+        addvm
+        #devour alacritty -e $choice
         ;;
     Quit)
         exit 1
@@ -25,7 +45,6 @@ case "$choice" in
         ;;
 esac
 
-#alacritty -e "$choice"
 
 
 
